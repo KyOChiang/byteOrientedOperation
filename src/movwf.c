9af7437007dee3a2f21711b15aa42a8cf13758dd
KyO <yong931231@hotmail.com>
2014-04-03 13:48:10 +0800

    Add test for movwf to verify range within operand1. If op1 > 0x7F, it should move value in WREG to (0xF00+operand1) addr. Otherwise, move the value to (operand1) file reg addr.

--- src/movwf.c
+++ src/movwf.c
@@ -8,7 +8,10 @@ char FSR[0x1000];
 void movwf(Bytecode *code){
 
 	if(code->operand1 > 0x00 && code->operand1 <= 0xFF){
-		FSR[code->operand1] = FSR[WREG];
+		if(code->operand1>=0x80)
+			FSR[code->operand1+0xF00] = FSR[WREG];
+		if(code->operand1<0x80)
+			FSR[code->operand1] = FSR[WREG];
 	}
 	else
 		Throw(ERR_INVALID_OPERAND1);
--- test/test_movwf.c
+++ test/test_movwf.c
@@ -28,3 +28,25 @@ void test_movwf_should_move_content_inside_WREG_to_selected_file_register(){
 	TEST_ASSERT_EQUAL_HEX8(0x45,FSR[code.operand1]);
 }
 
+void test_movwf_with_file_addr_more_than_0x7F_should_move_content_inside_WREG_to_BANKED_file_register(){
+	CEXCEPTION_T errorStatus;
+	//Test fixture
+	
+	Bytecode code = {.instruction = {.mnemonic = MOVWF, .name = "movwf"},
+					 .operand1 = 0x80,
+					 .operand2 = -1,
+					 .operand3 = -1
+					};
+					
+	//Initialize WREG with value 0x45				
+	FSR[WREG] = 0x45;
+	
+	Try{
+		movwf(&code);
+	}Catch(errorStatus){
+		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND1, errorStatus);
+	}
+	
+	TEST_ASSERT_EQUAL_HEX8(0x45,FSR[code.operand1+(0xF00)]);
+}
+
