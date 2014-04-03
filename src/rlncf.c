400aa1a5fd303e86e00378197313901e599b97d8
KyO <yong931231@hotmail.com>
2014-04-03 21:05:16 +0800

    Add test for rlncf to verify operand2. To indicate where data store after execute rlncf, WREG or FileREG.

--- src/rlncf.c
+++ src/rlncf.c
@@ -17,10 +17,20 @@ void rlncf(Bytecode *code){
 	if(code->operand1 > 0x00 && code->operand1 <= 0xFF){
 		//To check that bit 7 is 1 or not, because is a signed char
 		//If bit 7 is 1, the value will read as negative value
-		if(FSR[code->operand1]<0)
-			FSR[code->operand1] = (FSR[code->operand1]<<1) + 0x01;
-		else
-			FSR[code->operand1] = (FSR[code->operand1]<<1) + 0x00;
+		if(FSR[code->operand1]<0){
+			if(code->operand2 == F||code->operand2 == 1||code->operand2 == -1){
+				FSR[code->operand1] = (FSR[code->operand1]<<1) + 0x01;
+			}
+			else if(code->operand2 == W||code->operand2 == 0)
+				FSR[WREG] = (FSR[code->operand1]<<1) + 0x01;
+		}
+		else{
+			if(code->operand2 == F||code->operand2 == 1||code->operand2 == -1){
+				FSR[code->operand1] = (FSR[code->operand1]<<1) + 0x00;
+			}
+			else if(code->operand2 == W||code->operand2 == 0)
+				FSR[WREG] = (FSR[code->operand1]<<1) + 0x00;
+		}
 	}
 	else
 		Throw(ERR_INVALID_OPERAND1);
--- test/test_rlncf.c
+++ test/test_rlncf.c
@@ -63,3 +63,47 @@ void test_rlncf_should_not_rotate_to_left_without_carry_with_invalid_operand1_0x
 		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND1, errorStatus);
 	}
 }
+
+void test_rlncf_should_rotate_to_left_without_carry_and_store_inside_filereg(){
+	CEXCEPTION_T errorStatus;
+	//Test fixture
+	
+	Bytecode code = {.instruction = {.mnemonic = RLNCF, .name = "rlncf"},
+					 .operand1 = 0x10,
+					 .operand2 = F,
+					 .operand3 = -1
+					};
+					
+	//Initialize FSR[code.operand1] with value 0xA4	- 1010 0100	
+	//After rotate to left, become 0100 1001 - 49
+	FSR[code.operand1] = 0xA4;
+	Try{
+		rlncf(&code);
+	}Catch(errorStatus){
+		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND1, errorStatus);
+	}
+	
+	TEST_ASSERT_EQUAL_HEX8(0x49,FSR[code.operand1]);
+}
+
+void test_rlncf_should_rotate_to_left_without_carry_and_store_inside_WREG(){
+	CEXCEPTION_T errorStatus;
+	//Test fixture
+	
+	Bytecode code = {.instruction = {.mnemonic = RLNCF, .name = "rlncf"},
+					 .operand1 = 0x10,
+					 .operand2 = W,
+					 .operand3 = -1
+					};
+					
+	//Initialize FSR[code.operand1] with value 0xA4	- 1010 0101	
+	//After rotate to left, become 0100 1011 - 4B
+	FSR[code.operand1] = 0xA5;
+	Try{
+		rlncf(&code);
+	}Catch(errorStatus){
+		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND1, errorStatus);
+	}
+	
+	TEST_ASSERT_EQUAL_HEX8(0x4B,FSR[WREG]);
+}
